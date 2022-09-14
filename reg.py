7ea24a3809bf727bf74271f3e445536ba3c7c692
anatcohen <anatco13@gmail.com>
2022-09-14 16:21:25 +0200

    added optimisation

--- reg.py
+++ reg.py
@@ -1,69 +1,113 @@
-# Input: cl, ebsd images
-# Get initial histogram
-# Create loss function
-
 import numpy as np
+from torch import cumsum, abs, sum
+import hist_methods
+import scipy.optimize as opt
 import cv2
-from pyemd import emd
-import fixed_hist
 
-# Problems/ Questions:
-# What is the distance matrix in pyemd?
-
-
-def best_transformation(mask_img, corners, min_emd, trans_mat, step):
-    # Stop condition
-
-    # north-west, north-east, south-west, south-east corners
-    nw, ne, sw, se = corners
-
-    trans_matrix = cv2.getAffineTransform(pts1, pts2)
-    trans_img = cv2.warpAffine(cl_img, trans_matrix, (cols, rows))
-    trans_his = fixed_hist.get_colours_by_coords(trans_img, mask_img)
-
-    new_emd = calc_emd(trans_his)
-
-    # Replace min emd
-    if new_emd < min_emd:
-        min_emd = new_emd
-        trans_mat = trans_matrix
-
-    # New points in range
-    if nw in [rows, cols] and ne in [rows, cols] and sw in [rows, cols] and se in [rows, cols]:
-        # Update corners
-        return best_transformation(corners, min_emd, step)
-
-    # return best_transformation(pts1, pts2 + step, step)
-    return trans_mat
+# Problems/Questions:
+# 1. Will one frequent colour be enough?
+# 2. Find how to get the right range of frequent colours
+# 3. Find most accurate method
 
 
-# Cite Ofir Pele and Michael Werman's pyemd paper
-# What is the distance matrix?
-def calc_emd(his1):
-    distance_matrix = np.array([[0.0, 0.5], [0.5, 0.0]])
-    return emd(his1, his1, distance_matrix)
+def calc_emd(p):
+    x = ref_his - p
+    y = cumsum(x, dim=0)
+    return abs(y).sum()
+
+
+def old_idea():
+    # def find_best_transformation(points, step, min_emd=None, min_trans=None):
+    #     # General idea:
+    #     # Fix three points at a time by order:
+    #     # 1. Fix nw corner
+    #     # 2. Fix ne corner
+    #     # 3. Fix sw corner
+    #     # 4. Fix se corner
+    #     # 5. Calc emd
+    #     # 6. Repeat 4 after and find min emd
+    #     # 7. Repeat 3 and find min emd
+    #     # 8. Repeat 2 and find min emd
+    #     # 9. Repeat 1 and find min emd
+    #
+    #     nw = points[0]  # north-west, north-east, south-west, south-east corners
+    #
+    #     if nw >= height:
+    #         return min_trans
+    #
+    #     new_emd, new_mat = ne_transformation(points, step, min_emd, min_trans)
+    #
+    #     if new_emd < min_emd:
+    #         min_emd = new_emd
+    #         min_mat = new_mat
+    #
+    #     points[0] += step
+    #     return find_best_transformation(points, step, min_emd, min_mat)
+
+    # def ne_transformation(points, step, min_emd, min_mat):
+    #     if points[1] > width:
+    #         return min_emd, min_mat
+    #
+    #     new_emd, new_mat = sw_transformation(points, min_emd, min_mat)
+    #
+    #     if new_emd < min_emd:
+    #         min_emd = new_emd
+    #         min_mat = new_mat
+    #
+    #     points[1] += step
+    #
+    #     return ne_transformation(points, step, min_emd, min_mat)
+    #
+    #
+    # def sw_transformation(points, step, min_emd, min_mat):
+    #     # if points[2] <
+    #     new_emd, new_mat = se_transformation(points, step, min_emd, min_mat)
+    #     if new_emd < min_emd:
+    #         min_emd = new_emd
+    #         min_mat = new_mat
+    #
+    #     points[2] += step
+    #     return sw_transformation()
+    #
+    #
+    # def se_transformation(points, step, min_emd, min_mat):
+    #     se = points[4]
+    #     if se > width:
+    #         return min_mat
+    #
+    #     trans_mat = cv2.getPerspectiveTransform(*points)
+    #     warp_img = cv2.warpPerspective(cl_img, trans_mat, (width, height))
+    #     mask_img = ''
+    #     trans_his = fixed_hist.get_colours_by_coords(warp_img, mask_img)
+    #     new_emd = calc_emd(trans_his)
+    #
+    #     if min_emd is None or new_emd < min_emd:
+    #         min_emd = new_emd
+    #         min_mat = trans_mat
+    #
+    #     points[4] += step
+    #     return se_transformation(points, step, min_emd, min_mat)
+    pass
+
+
+def loss_func(points):
+    trans_mat = cv2.getPerspectiveTransform(*points)
+    trans_cl = cv2.warpPerspective(cl_img, trans_mat, (width, height))
+    trans_his = hist_methods.get_hist(trans_cl, ebsd_img, ref_freq_col, ref_freq_col - 50, ref_freq_col + 50)
+    return calc_emd(trans_his)
+
+
+def find_best_transformation():
+    trans = opt.minimize(calc_emd, [[0, 0], [0, 1024], [1024, 0], [1024, 1024]])
+    print(trans)
 
 
 if __name__ == "__main__":
     # Load Images
-    # cl_img, ebsd_img, quant_image = fixed_hist.load_images()
+    cl_img, ebsd_img, quant_image = hist_methods.load_images()
     # Get reference histogram and reference frequent colours
-    ref_freq_col, ref_his = fixed_hist.get_basic_data()
-    # mask_img = fixed_hist.get_mask_coordinates(ebsd_img, ref_freq_col - 30, ref_freq_col + 30)
+    ref_freq_col, ref_his = hist_methods.get_basic_data()
+    height, width, ch = cl_img.shape
 
-    # rows, cols, ch = cl_img.shape
-    # pts1 = np.float32([[0, 0], [rows, 0], [rows, cols], [0, cols]])
-    # pts2 = np.float32([[0, 100], [rows - 100, 0], [rows, cols], [0, cols]])
+    find_best_transformation()
 
-    # M = cv2.getPerspectiveTransform(pts1, pts2)
-    # warp = cv2.warpPerspective(cl_img, M, (cols, rows))
-
-    cl = cv2.imread('cl.png')
-    ebsd = cv2.imread('ebsd.bmp')
-    print(ref_freq_col)
-    mask_img = fixed_hist.get_mask_coordinates(ebsd, ref_freq_col - 50, ref_freq_col + 50)
-    coords = np.column_stack(np.where(mask_img > 0))
-
-    cv2.imshow('', mask_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
