6ce483ba8e4c856cf55581fb2016a3eeab635bf7
Anat Cohen <55288181+anatcohen@users.noreply.github.com>
2022-10-06 16:53:42 +0200

    Fixed loss function

--- reg.py
+++ reg.py
@@ -11,20 +11,22 @@ truth_path = 'data/affine_reg.png'
 ebsd_path = 'Sequence/EBSD_09-07-29_1415-13_Map1-2-3-4_corr-BC-IPF-GB_crop.png'
 orig_cl_path = 'Sequence/cl.png'
 
-
 # Transformation boundaries
-SUP_TRANSLATE_X = 120
-SUP_TRANSLATE_Y = 110
-SUP_ROTATION = 15
+INF_TRANSLATE_X = -120
+SUP_TRANSLATE_X = 0
+INF_TRANSLATE_Y = -120
+SUP_TRANSLATE_Y = 0
+INF_ROTATION = -15
+SUP_ROTATION = 0
 INF_SCALE_X = 0.8
 SUP_SCALE_X = 1.5
 INF_SCALE_Y = 0.8
 SUP_SCALE_Y = 1.5
 
 # Initial bounds
-TRANS_X_BOUNDS = (-120, 120, 10)
-TRANS_Y_BOUNDS = (-120, 120, 10)
-ROT_BOUNDS = (-15, 15, 3)
+TRANS_X_BOUNDS = (-150, 0, 20)
+TRANS_Y_BOUNDS = (-150, 0, 20)
+ROT_BOUNDS = (-15, 0, 3)
 SCALE_X_BOUNDS = (0.8, 1.2, 0.2)
 SCALE_Y_BOUNDS = (0.8, 1.2, 0.2)
 
@@ -38,8 +40,8 @@ STEP = 2
 # 9. Change boundary constant to (-sup, sup, step) form
 # 15. Implement multiprocessing
 # 17. plot losses
-# 18. make sure skew = 0 is  not possible
 # 19. bad skewing values result in a black image
+# 20. Add resizing option for when original images aren't the same size
 
 
 # Loads cl and ebsd images by path
@@ -53,12 +55,14 @@ def load_images():
 def create_canny_img(img):
     grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.GaussianBlur(grey_img, (5, 5), 0)
-    # quant = quantise_img(img, 8)
 
-    # high_thresh, thresh_im = cv2.threshold(quant, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     high_thresh, thresh_im = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     low_thresh = 0.5 * high_thresh
     edges = cv2.Canny(img, low_thresh, high_thresh)
+
+    # Thicken the borders
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    edges = cv2.dilate(edges, kernel, iterations=2)
     return edges
 
 
@@ -67,11 +71,9 @@ def get_canny_images():
     # Canny Images
     ebsd_canny = create_canny_img(ebsd_img)
     cl_canny = create_canny_img(cl_img)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    ebsd_canny = cv2.dilate(ebsd_canny, kernel, iterations=2)
+    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    # ebsd_canny = cv2.dilate(ebsd_canny, kernel, iterations=2)
     return ebsd_canny, cl_canny
-    # cl_dil_canny = cv2.dilate(cl_canny, kernel, iterations=2)
-    # return ebsd_dil_canny, cl_dil_canny
 
 
 # Returns canny image of cl after having had given transformation applied to it
@@ -79,12 +81,10 @@ def get_canny_images():
 def create_trans_img(trans, cl_canny, dim):
     width, height, cX, cY = dim
     translate_x, translate_y, degree, scale_x, scale_y = trans
-    # translate_x, translate_y, degree = trans
-    scale_x = scale_y = 1
+
     # Translation
     translation_mat = np.float32([[scale_x, 0, translate_x], [0, scale_y, translate_y]])
     img = warpAffine(cl_canny, translation_mat, (width, height))
-    # cv2.imshow('0', img)
 
     # Rotation
     rot_mat = getRotationMatrix2D((cX, cY), degree, 1.0)
@@ -95,7 +95,7 @@ def create_trans_img(trans, cl_canny, dim):
 # Converts image array to a 1D binary array of {0,1}
 def convert_canny_to_bi(img):
     flat_arr = img.flatten()
-    # Replace black pixels with 1 and white with 1
+    # Replace black pixels with 0 and white with 1
     flat_arr[flat_arr == 0] = 1
     flat_arr[flat_arr != 1] = 0
     return flat_arr
@@ -163,15 +163,15 @@ def optimiser(n=1, trans_x_bounds=TRANS_X_BOUNDS, trans_y_bounds=TRANS_Y_BOUNDS,
 # Defines new boundaries for next generation of possible transformations
 def get_new_boundaries(trans, trans_x_step, trans_y_step, rot_step, scale_x_step, scale_y_step):
     x, y, deg, scale_x, scale_y = trans
-    new_trans_x_inf = max(x-trans_x_step, -SUP_TRANSLATE_X)
+    new_trans_x_inf = max(x-trans_x_step, INF_TRANSLATE_X)
     new_trans_x_sup = min(x+trans_x_step, SUP_TRANSLATE_X)
     new_trans_x_bounds = (new_trans_x_inf, new_trans_x_sup, trans_x_step//2)
 
-    new_trans_y_inf = max(y-trans_y_step, -SUP_TRANSLATE_Y)
+    new_trans_y_inf = max(y-trans_y_step, INF_TRANSLATE_Y)
     new_trans_y_sup = min(y+trans_y_step, SUP_TRANSLATE_Y)
     new_trans_y_bounds = (new_trans_y_inf, new_trans_y_sup, trans_y_step//2)
 
-    new_rot_inf = max(deg-rot_step, -SUP_ROTATION)
+    new_rot_inf = max(deg-rot_step, INF_ROTATION)
     new_rot_sup = min(deg+rot_step, SUP_ROTATION)
     new_rot_bounds = (new_rot_inf, new_rot_sup, trans_y_step//2)
 
@@ -190,9 +190,10 @@ def get_new_boundaries(trans, trans_x_step, trans_y_step, rot_step, scale_x_step
 def get_arr_loss(trans_arr):
     # Zipping the global arguments for multiprocessing to work
     cl_arr = [cl_canny]*len(trans_arr)
-    bi_arr = [bi_ebsd]*len(trans_arr)
+    # bi_arr = [bi_ebsd]*len(trans_arr)
+    ebsd_arr = [ebsd_canny]*len(trans_arr)
     dim = [(width, height, cX, cY)]*len(trans_arr)
-    args = list(zip(trans_arr, cl_arr, bi_arr, dim))
+    args = list(zip(trans_arr, cl_arr, ebsd_arr, dim))
 
     with Pool(mp.cpu_count() - 1) as pool:
         results = pool.starmap(loss_func, args)
@@ -200,15 +201,24 @@ def get_arr_loss(trans_arr):
     # return np.array([loss_func(trans) for trans in trans_arr])
 
 
-# Sums the multiplication of the transposed canny and ebsd canny. The higher, the better
+# Old loss func
 # def loss_func(trans):
-def loss_func(trans, cl_canny, bi_ebsd, dim):
+# def loss_func(trans, cl_canny, bi_ebsd, dim):
     # trans_img = create_trans_img(trans)
-    trans_img = create_trans_img(trans, cl_canny, dim)
-    bi_trans = convert_canny_to_bi(trans_img)
-    loss = np.sum(bi_ebsd*bi_trans)
+    # trans_img = create_trans_img(trans, cl_canny, dim)
+    # bi_trans = convert_canny_to_bi(trans_img)
+    # loss = np.sum(bi_ebsd*bi_trans)
     # if sum(bi_ebsd) - loss < 25:
     #     loss = 0
+    # return loss*-1
+
+# Sums the multiplication of the transposed canny and ebsd canny. The higher, the better
+def loss_func(trans, cl_canny, ebsd_canny, dim):
+    trans_canny = create_trans_img(trans, cl_canny, dim)
+    inter = ebsd_canny & trans_canny
+    flat_arr = inter.flatten()
+    flat_arr[flat_arr != 0] = 1
+    loss = np.sum(flat_arr)
     return loss*-1
 
 # Simulated Annealing Optimiser LOSS function
@@ -276,44 +286,13 @@ if __name__ == "__main__":
     # Load Images
     ebsd_img, cl_img = load_images()
     ebsd_canny, cl_canny = get_canny_images()
-    bi_ebsd = convert_canny_to_bi(ebsd_canny)
+    # bi_ebsd = convert_canny_to_bi(ebsd_canny)
 
     # Image dimensions
     height, width = cl_canny.shape
     (cX, cY) = (width // 2, height // 2)
+    dim = (width, height, cX, cY)
 
-    # Custom Scheduled Optimisation
-    transformation = find_best_trans(3)
-    print(transformation)
-
-    # Basin-Hopping Optimisation
-    # c1 = {'type': 'ineq', 'fun': lambda x: 1.5 - x[4]}
-    # c2 = {'type': 'ineq', 'fun': lambda x: x[4] - 0.8}
-    # c3 = {'type': 'ineq', 'fun': lambda x: 1.5 - x[3]}
-    # c4 = {'type': 'ineq', 'fun': lambda x: x[3] - 0.8}
-    # minimizer_kwargs = {"method": "COBYLA", "constraints": (c1, c2, c3, c4)}
-    # res = basinhopping(loss_func, (0, 0, 0, 1, 1), niter=100, minimizer_kwargs=minimizer_kwargs)
-    # print(res)
-    # [-1.97266734e-05,  9.99939793e-01,  6.40602132e-05,  1.00000373e+00, 1.00004322e+00] loss: -638301
-
-    # Simulated Annealing Optimisation
-    # inf = [-200, -200, -45]
-    # sup = [200, 200, 45]
-    # res = dual_annealing(loss_func, bounds=list(zip(inf, sup)))
-    # print(res)
-    # res w scaling: [-189.90122424, -193.72709634, 19.46906527, 0.83225369, 0.85481252] loss: -643987
-    # res w/o scaling: [-190.99317145, -198.98928199,  -36.87111562] loss: -643062
-
-    # Simulated Annealing Optimisation on perspective warp
-    # inf = [0, 0, width*(2/3), 0, 0, height*(2/3), width*(2/3), height*(2/3)]
-    # sup = [width//3, height//3, width, height//3, width//3, height, width, height]
-    # res = dual_annealing(loss_func, bounds=list(zip(inf, sup)))
-    # print(res)
-    # res: [9.36962564, 298.96559851, 1018.85279349, 305.04906874,
-    #         297.85357828,  962.34589884,  709.63185314,  957.34115835] loss: -636933
-    # coord1 = np.array([[9.36962564, 298.96559851], [1018.85279349, 305.04906874], [297.85357828, 962.34589884], [709.63185314, 957.34115835]], np.float32)
-    # coord2 = np.array([[0, 0], [0, width], [height, 0], [height, width]], np.float32)
-    # mat = cv2.getPerspectiveTransform(coord1, coord2)
-    # trans_img = cv2.warpPerspective(cl_img, mat, cl_canny.shape)
-    # cv2.imshow('', trans_img)
-    # cv2.waitKey(0)
+    # Truth loss: -144333
+    # transformation = find_best_trans()
+    # print(transformation)
