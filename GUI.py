98baee0c403e7914d92f8adff0da2a5be479fd72
Anat Cohen <55288181+anatcohen@users.noreply.github.com>
2022-10-07 15:24:32 +0200

    Added correlation GUI

--- GUI.py
+++ GUI.py
@@ -2,24 +2,21 @@ import PySimpleGUI as sg
 import os.path
 import io
 from PIL import Image
-
-# file_types = [("JPEG (*.jpg)", "*.jpg", "*j.peg", "*.jpe"), ("JPEG 2000 (*.jpg)", "*.jp2",), ("BMP (*.bmp)", "*.bmp"),
-#               ("PBM (*pbm)", "*.pbm", "*.pgm", "*.ppm"), ("SUNRASTER (*.sr)", "*.sr", "*.ras"),
-#               ("TIFF (*.tiff)", "*.tiff", "*.tif"), ("PNG (*.png)", "*.png")]
-
-
-# file_types = [("*.jpg", "*.jpeg", "*.jpe"), ("JPEG 2000", "*.jp2",), ("BMP", "*.bmp"),
-#               ("PBM", "*.pbm", "*.pgm", "*.ppm"), ("SUNRASTER", "*.sr", "*.ras"), ("TIFF", "*.tiff", "*.tif"),
-#               ("PNG", "*.png")]
+import matplotlib as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import cor
 
 IMG_UPLOAD_LAYOUT = '-IMAGE_LAY-'
+LOAD_LAYOUT = '-LOAD_LAY-'
 COR_LAYOUT = '-COR_LAY-'
 REG_LAYOUT = '-REG_LAY-'
 
 img_upload_vis = True
+load_vis = False
 cor_vis = False
-reg_vis = True
+reg_vis = False
 
+# Upload CL&EBSD data layout
 cl_col = [
     [sg.Text("Upload CL Image")],
     [
@@ -28,7 +25,6 @@ cl_col = [
     ],
     [sg.Image(key="-CL_IMG-")]
 ]
-
 ebsd_col = [
     [sg.Text("Upload EBSD Image:")],
     [
@@ -37,9 +33,7 @@ ebsd_col = [
     ],
     [sg.Image(key="-EBSD_IMG-")]
 ]
-
 submit_col = [[sg.Button('Register', key="-REGISTER-")], [sg.Button('Correlate', key='-CORRELATE-')]]
-
 image_upload_layout = [
     [
         # sg.Column(image_load, key=image_load)
@@ -50,39 +44,70 @@ image_upload_layout = [
     ]
 ]
 
-cor_layout = [[sg.Text('Test')]]
+# Loading screen layout
+loading_layout = [[sg.Text('Loading...')]]
 
+# Correlation screen layout
+cor_lay = [
+    [sg.InputText(key='-SAVE_AS-', do_not_clear=False, enable_events=True, visible=False), sg.FileSaveAs()],
+    [sg.Canvas(key="-CANVAS-")]
+]
+
+# Registration screen layout
 reg_layout = [[]]
 
+# General layout
 layout = [
     [
         sg.Column(image_upload_layout, key=IMG_UPLOAD_LAYOUT),
-        sg.Column(cor_layout, key=COR_LAYOUT, visible=False),
-        sg.Column(cor_layout, key=REG_LAYOUT, visible=False)
+        sg.Column(loading_layout, key=LOAD_LAYOUT, visible=False),
+        sg.Column(cor_lay, key=COR_LAYOUT, visible=False),
+        sg.Column(reg_layout, key=REG_LAYOUT, visible=False)
     ]
 ]
-
 window = sg.Window(IMG_UPLOAD_LAYOUT, layout)
+
+# Enables matplotlib graphs
+plt.use("TkAgg")
+fig = plt.figure.Figure(figsize=(5, 4), dpi=100)
+
+
+# Draws matplotlib graphs on canvas
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
+    return figure_canvas_agg
+
 
 while True:
     event, values = window.read()
     # Terminate window
     if event == sg.WIN_CLOSED or event == "Exit":
         break
+    # Switch to register screen
     elif event == "Register":
         print('done')
+    # Switch to correlation screen
     elif event == "-CORRELATE-":
-        # window = sg.Window(CORRELATION_LAYOUT, cor_layout)
         window[IMG_UPLOAD_LAYOUT].update(visible=False)
         img_upload_vis = False
         cor_vis = True
-        print( window[IMG_UPLOAD_LAYOUT].)
         window[COR_LAYOUT].update(visible=True)
+        fig = cor.get_fig(ebsd_path, cl_path)
+        draw_figure(window["-CANVAS-"].TKCanvas, fig)
+    # Save correlation histograms
+    elif event == '-SAVE_AS-':
+        filename = values['-SAVE_AS-']
+        if filename:
+            fig.savefig(filename)
+
     cl_path = values["-CL_FILE-"]
     ebsd_path = values["-EBSD_FILE-"]
 
-    # Displays selected CL image
+    # Image upload screen
     if img_upload_vis:
+        # Displays selected CL image
         if os.path.exists(cl_path):
             image = Image.open(cl_path)
             image.thumbnail((400, 400))
@@ -99,5 +124,3 @@ while True:
         # Displays register button once both CL and EBSD images have been selected
         if os.path.exists(ebsd_path) and os.path.exists(cl_path):
             window['-SUBMIT-'].Update(visible=True)
-
-
